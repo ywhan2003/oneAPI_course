e42e6e77becdc1c21008e893e03969abf0d39915
ywhan2003 <2110520134@qq.com>
2024-11-09 17:07:21 +0800

    modify basic_parafor.cpp

--- code/basic_parafor.cpp
+++ code/basic_parafor.cpp
@@ -7,29 +7,33 @@
    constexpr int N = 10;
    
    int main() {
-    //  queue my_gpu_queue( sycl::cpu_selector_v);
-     queue my_gpu_queue( sycl::gpu_selector_v);
+      queue my_gpu_queue( sycl::cpu_selector_v);
+    // queue my_gpu_queue( sycl::gpu_selector_v);
    
      std::cout << "Selected GPU device: " <<
        my_gpu_queue.get_device().get_info<info::device::name>() << "\n";
    
-     int *host_mem   = malloc_host<int>(N, my_gpu_queue);
-     int *device_mem = malloc_device<int>(N, my_gpu_queue); 
+    //  int *host_mem   = malloc_host<int>(N, my_gpu_queue);
+    //  int *device_mem = malloc_device<int>(N, my_gpu_queue); 
+
+    int *shared_mem = malloc_shared<int>(N, my_gpu_queue);
    
      // Init CPU data
      for(int i = 0; i < N; i++) {
-        host_mem[i] = i;
+        // host_mem[i] = i;
+        shared_mem[i] = i;
      }
    
      // Copy from host(CPU) to device(GPU)
-     my_gpu_queue.memcpy(device_mem, host_mem, N * sizeof(int)).wait();
+     // my_gpu_queue.memcpy(device_mem, host_mem, N * sizeof(int)).wait();
    
      // submit the content to the queue for execution
      my_gpu_queue.submit([&](handler& h) {
        
        // Parallel Computation      
        h.parallel_for(range{N}, [=](id<1> item) {
-         device_mem[item] *= 2;
+         // host_mem[item] *= 2;
+         shared_mem[item] *= 2;
        });
 
      });
@@ -38,17 +42,18 @@
      my_gpu_queue.wait();
    
      // Copy back from GPU to CPU
-     my_gpu_queue.memcpy(host_mem, device_mem, N * sizeof(int)).wait();
+     // my_gpu_queue.memcpy(host_mem, device_mem, N * sizeof(int)).wait();
 
      printf("\nData Result\n");
      for(int i = 0; i < N; i++) {
-        printf("%d, ", host_mem[i]);
+        // printf("%d, ", host_mem[i]);
+        printf("%d, ", shared_mem[i]);
      }
      printf("\nTask Done!\n");
       
-     free(host_mem, my_gpu_queue);
-     free(device_mem, my_gpu_queue);  
-   
+     // free(host_mem, my_gpu_queue);
+     // free(device_mem, my_gpu_queue);  
+     free(shared_mem, my_gpu_queue);
      return 0;
    }
 
